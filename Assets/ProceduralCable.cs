20e7f31df1e599ea5bea0cebfac3104cca9753fa
fleuvesauvage <41638621+fleuvesauvage@users.noreply.github.com>
2018-09-07 06:30:03 +0200

    Project base

---
+++ Assets/ProceduralCable.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ProceduralCable : MonoBehaviour {
+
+    public Vector3 a;
+    public Vector3 b;
+    public int step = 20;
+    public float curvature = 1;
+    public float radius = 0.2f;
+    public int radiusStep = 6;
+
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+    void OnEnable() {
+
+        Debug.Log("Start");
+        meshFilter = GetComponent<MeshFilter>() == null ? gameObject.AddComponent<MeshFilter>() : GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>() == null ? gameObject.AddComponent<MeshRenderer>() : GetComponent<MeshRenderer>();
+        UpdateObject();
+
+    }
+
+	void Update () {
+		
+	}
+
+    public float CurveHeight(int i)
+    {
+        i = Mathf.Clamp(i,0,step);
+        float normalizedStep = (float)i / step;
+        return (Mathf.Pow((normalizedStep * 2) - 1, 2) - 1)*curvature;
+    }
+
+    public Vector3 PointPosition(int i)
+    {
+        Vector3 segment = (b - a) / step;
+        return a + segment * i + new Vector3(0,CurveHeight(i),0);
+    }
+
+    public Vector3[] VerticesForPoint(int i)
+    {
+        Vector3 pointPosition = PointPosition(i);
+        Vector3 orientation;
+
+        if (i == 0)
+            orientation = PointPosition(1) - PointPosition(0);
+        else if(i == step)
+            orientation = PointPosition(step) - PointPosition(step-1);
+        else
+            orientation = PointPosition(i + 1) - PointPosition(i - 1);
+
+        Quaternion rotation = Quaternion.LookRotation(orientation, Vector3.Cross(Vector3.down, b - a)); 
+
+        List<Vector3> vertices = new List<Vector3>();
+        float angleStep = 360f / radiusStep;
+
+        for(int h = 0; h < radiusStep; h++)
+        {
+            float angle = angleStep * h * Mathf.Deg2Rad;
+            vertices.Add(pointPosition + rotation * (new Vector3( Mathf.Cos(angle)*radius , Mathf.Sin(angle)*radius , 0 )));
+        }
+
+        return vertices.ToArray();
+    }
+
+    public void UpdateObject()
+    {
+        meshFilter.sharedMesh = GenerateMesh();
+    }
+
+    public Mesh GenerateMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "Cable mesh";
+
+        List<Vector3> vertices = new List<Vector3>();
+        List<int> triangles = new List<int>();
+
+        for (int i = 0; i <= step; i++)
+        {
+            Vector3[] verticesForPoint = VerticesForPoint(i);
+            for (int h = 0; h < verticesForPoint.Length; h++)
+            {
+                vertices.Add(verticesForPoint[h]);
+
+                if (i < step)
+                {
+                    int index = h + (i * radiusStep);
+
+                    triangles.Add(index);
+                    triangles.Add(index + 1);
+                    triangles.Add(index + radiusStep);
+
+                    triangles.Add(index);
+                    triangles.Add(index + radiusStep);
+                    triangles.Add(index + radiusStep - 1);
+
+                }
+            }
+        }
+
+        mesh.vertices = vertices.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        
+        return mesh;
+    }
+
+}
---
+++ Assets/ProceduralCableInspector.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ProceduralCable))]
+public class ProceduralCableInspector : Editor {
+
+    ProceduralCable proceduralCable;
+
+    public void OnEnable()
+    {
+        proceduralCable = (ProceduralCable)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        float newCurvature = EditorGUILayout.FloatField("Curvature", proceduralCable.curvature);
+        int newStep = EditorGUILayout.IntField("Step",proceduralCable.step);
+        int newRadiusStep = EditorGUILayout.IntField("Radius step", proceduralCable.radiusStep);
+        float newRadius = EditorGUILayout.FloatField("Radius", proceduralCable.radius);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(proceduralCable, "Change parameter");
+
+            proceduralCable.curvature = newCurvature;
+
+            newStep = newStep < 1 ? 1 : newStep;
+            proceduralCable.step = newStep;
+
+            newRadiusStep = newRadiusStep < 3 ? 3 : newRadiusStep;
+            proceduralCable.radiusStep = newRadiusStep;
+
+            newRadius = newRadius < 0 ? 0 : newRadius;
+            proceduralCable.radius = newRadius;
+
+            proceduralCable.UpdateObject();
+
+            EditorUtility.SetDirty(proceduralCable);
+        }
+    }
+
+    private void OnSceneGUI()
+    {
+        int step = proceduralCable.step;
+
+        EditorGUI.BeginChangeCheck();
+        Vector3 newAposition = Handles.DoPositionHandle(proceduralCable.a, Quaternion.identity);
+        Vector3 newBposition = Handles.DoPositionHandle(proceduralCable.b, Quaternion.identity);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(proceduralCable, "Change position of extremity point");
+            proceduralCable.a = newAposition;
+            proceduralCable.b = newBposition;
+            proceduralCable.UpdateObject();
+            EditorUtility.SetDirty(proceduralCable);
+        }
+        /*
+        for (int i = 0; i < step; i++)
+        {
+            Handles.DrawLine(proceduralCable.PointPosition(i), proceduralCable.PointPosition(i + 1));
+            DrawVerticesForPoint(i);
+        }
+        Handles.DrawPolyLine(proceduralCable.VerticesForPoint(step).ToArray());
+        DrawVerticesForPoint(step);*/
+
+    }
+
+    private void DrawVerticesForPoint(int i)
+    {
+        Vector3[] verticesForPoint = proceduralCable.VerticesForPoint(i);
+
+        for (int h = 0; h < verticesForPoint.Length-1; h++)
+            Handles.DrawLine(verticesForPoint[h], verticesForPoint[h + 1]);
+
+        Handles.DrawLine(verticesForPoint[proceduralCable.radiusStep-1], verticesForPoint[0]);
+    }
+
+}
